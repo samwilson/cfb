85567d3eff946de6cc400855a4b706cc9e70e33f
Sam Wilson <sam@samwilson.id.au>
2015-12-05 13:47:27 +0800

    Missed citation.

--- typeset.sh
+++ typeset.sh
@@ -1,13 +1,14 @@
 pdflatex main
 bibtex main
-makeindex main
 pdflatex main
+pdflatex main
+makeindex main
 pdflatex main
 
 #latex main
 #dvipdf main.dvi
 #dvips -o Main.ps Main.dvi
 #ps2pdf Main.ps
-rm *.ind *.idx *.ilg *.ent *.log *.aux *.toc *.out *.bbl *.blg
+rm *.ind *.idx *.ilg *.ent *.log *.aux *.toc *.out *.bbl *.blg *.bcf *.brf
 
 cp main.pdf ../gh-pages/book.pdf
