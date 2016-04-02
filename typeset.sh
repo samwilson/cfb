329b6c708b7aeddffce75e9edcdf0bcc269bf822
Sam Wilson <sam@samwilson.id.au>
2016-04-02 09:44:44 +0800

    Updated from Jan's info.

--- typeset.sh
+++ typeset.sh
@@ -9,6 +9,7 @@ pdflatex main
 #dvipdf main.dvi
 #dvips -o Main.ps Main.dvi
 #ps2pdf Main.ps
-rm *.ind *.idx *.ilg *.ent *.log *.aux *.toc *.out *.bbl *.blg *.bcf *.brf
+
+rm *.ind *.idx *.ilg *.log *.aux *.toc *.out *.bbl *.blg *.brf
 
 cp main.pdf ../gh-pages/book.pdf
